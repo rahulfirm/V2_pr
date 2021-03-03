efefaf92f4b284595993942a8812e98fa7e6fc75
parag-acellere <parag.dalal@embold.io>
2021-03-03 20:43:15 +0530

    Update DemoSubproject2.java

--- DemoSubproject2.java
+++ DemoSubproject2.java
@@ -1,7 +1,20 @@
 package subproject2;
 
 import org.subproject2_A.App;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Properties;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.text.StrSubstitutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonar.core.util.SettingFormatter;
 
 public class DemoSubproject2 { 
 
@@ -16,7 +29,13 @@ public class DemoSubproject2 {
 
 	public static DemoSubproject2 getFook() { 
 		
-		 
+		 try {
+      reader = new InputStreamReader(new FileInputStream(propertyFile), StandardCharsets.UTF_8);
+    } catch (Exception e) {
+      throw new IllegalStateException("Could not read properties from file: " + args[0], e);
+    } finally {
+      IOUtils.closeQuietly(reader);
+    }
 		if (fook==null) {
 			fook = new DemoSubproject2();
 		}                       
