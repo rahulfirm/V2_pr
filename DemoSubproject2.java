d8b363c4ef8ca86aac55db1236da0e74b391f144
parag-acellere <parag.dalal@embold.io>
2021-03-26 11:30:06 +0530

    Update DemoSubproject2.java

--- DemoSubproject2.java
+++ DemoSubproject2.java
@@ -2,39 +2,20 @@ package subproject2;
 
 import org.subproject2_A.App;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.charset.StandardCharsets;
-import java.util.Enumeration;
-import java.util.Map;
-import java.util.Properties;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.text.StrSubstitutor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.sonar.core.util.SettingFormatter;
-import java.security.SecureRandom;
-
 public class DemoSubproject2 { 
 
 	int k;
 	App app;
-	 public static final int BYTE_SIZE = 6;
 	
-   	new SecureRandom().nextBytes(mungedBytes);
 	public static void main(String[] args) {
-		
 		int k_subproject;
 	}
-	Reader reader = null;
+	
 	private static DemoSubproject2 fook = null;
 
 	public static DemoSubproject2 getFook() { 
-		byte[] address = null;
-		byte[] mungedBytes = new byte[BYTE_SIZE];
-		
+		 System.out.println(""); 
+		 
 		if (fook==null) {
 			fook = new DemoSubproject2();
 		}                       
@@ -53,13 +34,10 @@ public class DemoSubproject2 {
 		System.out.println("text");
 		try{
 			
-		System.out.println("inside try");
 		}
 		catch(Exception ex){
 			
-		System.out.println("error");
 		}
-		
 		
 	}
 	
