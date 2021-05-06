d4d61889a848f5459784b033c043c92cbde79e00
parag-acellere <parag.dalal@embold.io>
2021-05-06 18:22:19 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -5,7 +5,10 @@ import org.apache.logging.log4j.LogManager;
 
 class Simple{      
 public static void main(String args[]){      
- 
+ int param1=1;
+	 int param2=2;
  log.debug("log something" + " and " + "concat strings");
+	
+	log.debug("log something {} and {}", param1, param2);
  System.out.println("Hello Java");  
 }  } 
